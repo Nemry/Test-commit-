909ffb8344d31e7de5fdbecf5c0e6da238e6b766
Amine <Amine@LAPTOP-ND6QBQ6U.lajeunesse>
2023-05-11 00:33:30 -0400

    Commit Method Hello World

--- src/dépôt/local/DépôtLocal.java
+++ src/dépôt/local/DépôtLocal.java
@@ -10,11 +10,8 @@ package dépôt.local;
  */
 public class DépôtLocal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
     
 }
